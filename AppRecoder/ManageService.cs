00604c4d82699d6378badc66e847ae0c02be6b14
ndifix <yoshiki.linuxmint@gmail.com>
2020-10-10 02:36:50 +0900

    実行中の全てのプロセスをスレッドで監視できるようにした

--- AppRecoder/ManageService.cs
+++ AppRecoder/ManageService.cs
@@ -1,19 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AppRecoder
 {
 	class ManageService
 	{
+		private CancellationTokenSource cts = new CancellationTokenSource();
+
+		private Task watchProcessTask;
+
 		public void StartService()
 		{
 			Console.WriteLine("サービスを開始します。");
+
+			var processWatch = new ProcessWatch();
+			watchProcessTask = processWatch.WatchProcessAync(cts.Token);
 		}
 
 		public void StopService()
 		{
 			Console.WriteLine("サービスを終了します。");
+
+			if (!cts.IsCancellationRequested)
+			{
+				cts.Cancel();
+
+				try
+				{
+					if(watchProcessTask != null)
+					{
+						watchProcessTask.Wait();
+					}
+				}
+				catch(Exception ex)
+				{
+					Console.WriteLine($"サービスの停止に失敗。{ex.Message}");
+				}
+			}
 		}
 	}
 }
---
+++ AppRecoder/ProcessWatch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppRecoder
+{
+	public class ProcessWatch
+	{
+		private readonly Dictionary<string, string> currentProcess = new Dictionary<string, string>();
+
+		/// <summary>
+		/// 実行中の全てのプロセスを取得します。
+		/// </summary>
+		public async Task WatchProcessAync(CancellationToken cancellationToken)
+		{
+			Console.WriteLine("プロセスの監視スレッドを開始。");
+
+			while (!cancellationToken.IsCancellationRequested)
+			{
+				try
+				{
+					WatchProcessAsyncImpl();
+					await Task.Delay(60 * 1000, cancellationToken);
+				}
+				catch (TaskCanceledException)
+				{ }
+				catch (Exception ex)
+				{
+					Console.WriteLine($"プロセスの監視スレッドで未知の例外。{ex.Message}");
+				}
+			}
+
+			Console.WriteLine("プロセスの監視スレッドを終了。");
+		}
+
+		private void WatchProcessAsyncImpl()
+		{
+			Process[] processes = Process.GetProcesses();
+
+			lock (currentProcess)
+			{
+				currentProcess.Clear();
+
+				foreach (var process in processes)
+				{
+					try
+					{
+						currentProcess.Add(process.ProcessName, process.MainModule.FileName);
+					}
+					catch
+					{ }
+				}
+			}
+		}
+	}
+}
