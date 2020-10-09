e2e35c52f594e7664927d98d9e300ed966a5de48
ndifix <yoshiki.linuxmint@gmail.com>
2020-10-10 02:10:47 +0900

    Add project files.

---
+++ AppRecoder/ManageService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRecoder
+{
+	class ManageService
+	{
+		public void StartService()
+		{
+			Console.WriteLine("サービスを開始します。");
+		}
+
+		public void StopService()
+		{
+			Console.WriteLine("サービスを終了します。");
+		}
+	}
+}
---
+++ AppRecoder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace AppRecoder
+{
+	class Program
+	{
+		static void Main()
+		{
+			var service = new ManageService();
+			service.StartService();
+
+			Console.CancelKeyPress += (sender, e) =>
+			{
+				service.StopService();
+				Environment.Exit(0);
+			};
+			while (true)
+			{
+				Thread.Sleep(10);
+			}
+		}
+	}
+}
